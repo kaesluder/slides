baa1a16531567c8dada864a70910071b0c0b3693
Kae Sluder <kaesluder@fastmail.com>
2025-02-18 23:00:33 -0500

    working css and js slide implementation
    uses remark processor to wrap sections around headings

--- main.ts
+++ main.ts
@@ -1,21 +1,30 @@
 // @ts-types="npm:@types/express@4.17.15"
 import express, { Request, Response } from "npm:express@4.18.2";
 import vento from "jsr:@vento/vento";
-import { marked } from "npm:marked";
+import { remark } from "npm:remark";
+import html from "npm:remark-html";
+import sectionize from "npm:remark-sectionize";
 
 const env = vento();
 const app = express();
 
 const template = await env.load("./templates/slides.vto");
 
-export const processMarkdown = async (markdownFile: string) => {
+const markdownFile = "./slides/main.md"
+
+export const processMarkdown = async (markdownFile: string): Promise<string> => {
   const slidesMarkdown = await Deno.readTextFile(markdownFile);
-  return marked.parse(slidesMarkdown);
+  const file = await remark()
+    .use(sectionize)
+    .use(html, { sanitize: false })
+    .process(slidesMarkdown);
+
+  return String(file);
 };
 
 app.get("/", async (_: Request, res: Response) => {
-  const slidesMarkdown = await Deno.readTextFile("./slides/main.md");
-  const slidesHTML = await template({ markdown: marked.parse(slidesMarkdown) });
+  const slidesMarkdown = await processMarkdown(markdownFile); 
+  const slidesHTML = await template({ markdown: slidesMarkdown });
   console.log(slidesHTML.content);
   res.send(slidesHTML.content);
 });
--- slides/main.md
+++ slides/main.md
@@ -11,3 +11,16 @@ These operations can then be replayed on startup to re-create the current
 status of the stored dataset.
 
 The commands are logged in the same format as the Redis protocol itself.
+
+# Step 9 - Cooking
+
+
+In this step your goal is to make your Redis server persist its internal state 
+to disk and to reload the state on startup.
+
+For this step we will implement the AOF (Append Only File). In this mode the 
+Redis server logs every operation received by the server to disk. 
+These operations can then be replayed on startup to re-create the current 
+status of the stored dataset.
+
+The commands are logged in the same format as the Redis protocol itself.
